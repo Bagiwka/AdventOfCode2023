5469dbcaaf701a7e8d7d2fa4a71d5a332d6a2d1a
Bagiwka <sagivka2006@gmail.com>
2023-12-07 16:33:23 +0100

    Day4 part 2 not finished.

--- Day4/Cards.py
+++ Day4/Cards.py
@@ -10,27 +10,41 @@ for card in cards:
         continue
 
     cardParts = card.split('|')
-    cardID = cardParts[0].split(':')[0].strip()
+    cardID = cardParts[0].split(':')[0].strip().replace("Card","").replace(" ","")
     beforePipe = cardParts[0].split(':')[1].strip()
     afterPipe = cardParts[1].strip() if len(cardParts) > 1 else None
 
     cardData = {
         "ID": cardID,
         "Before": beforePipe,
-        "After": afterPipe
+        "After": afterPipe,
+        "Amount": 1
     }
 
     cardsData.append(cardData)
 
 for card in cardsData:
-    total = []
-    inputs = card["After"].split()
-    winners = card["Before"].split()
-    for number in inputs:
-        for win in winners:
-            if number == win:
-                total.append(number)
-    if total != []:
-        output += 2 ** (len(total)-1)
-        print(output)
-        print(total)
+    amount = 1
+    for repetition in range(0, amount, 1):
+        print(card)
+        total = 0
+        inputs = card["After"].split()
+        winners = card["Before"].split()
+        for number in inputs:
+            for win in winners:
+                if number == win:
+                    total += 1          
+        for i in range(1,total,1):
+            for element in cardsData:
+                if int(element["ID"]) == int(card["ID"])+i:
+                    cardsData[cardsData.index(element)]["Amount"] += 1
+        amount = cardsData[cardsData.index(element)]["Amount"]
+        print(amount)
+    print(total)
+
+finalAmount = 0
+for card in cardsData:
+    finalAmount += card["Amount"]
+
+print(finalAmount)
+#19855
